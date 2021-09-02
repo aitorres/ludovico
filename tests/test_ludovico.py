ad77f3bfc8457ff98b37ef530eaf5b5f9250f653
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-09-01 22:44:31 -0400

    test: add test for custom arguments on table generation function

--- tests/test_ludovico.py
+++ tests/test_ludovico.py
@@ -55,3 +55,58 @@ def test_generate_single_table() -> None:
         \\end{table}
         """
     )
+
+
+def test_generate_single_table_with_custom_args() -> None:
+    input_data = pd.DataFrame(
+        columns=["Column A", "Column B", "Column C"],
+        data=[
+            (
+                "entry 1",
+                10,
+                15.5,
+            ),
+            (
+                "entry 2",
+                15,
+                20.5,
+            ),
+            (
+                "entry 3",
+                20,
+                21.5,
+            ),
+            (
+                "entry 4",
+                12,
+                0.5,
+            ),
+        ],
+    )
+
+    assert (
+        generate_simple_table(
+            input_data,
+            table_name="Short Name",
+            table_long_name="This is a long table name",
+            table_label="label_1234",
+        )
+        == """
+        \\begin{table}
+            \\begin{center}
+            \\caption[Short Name]{This is a long table name}
+            \\label{tbl:label_1234}
+                \\begin{tabular}{rcl}
+                \\hline
+                Column A & Column B & Column C\\\\
+                \\hline
+        entry 1 & 10 & 15.5 \\\\
+        entry 2 & 15 & 20.5 \\\\
+        entry 3 & 20 & 21.5 \\\\
+        entry 4 & 12 & 0.5 \\\\
+                \\hline
+                \\end{tabular}
+            \\end{center}
+        \\end{table}
+        """
+    )
