53260ffed98ac9467f26aed2153453fe1668f036
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-09-02 01:26:30 -0400

    core: adjust setup file with extra info

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# TODO: move to setup.cfg
 setup(
     name="ludovico",
     version="0.0.1",
     description="An opinionated DataFrame-to-TeX table generator.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/aitorres/ludovico",
     author="Andrés Ignacio Torres",
     author_email="dev@aitorres.com",
@@ -17,17 +23,20 @@ setup(
             "pytest",
         ]
     },
+    project_urls={
+        "Bug Tracker": "https://github.com/aitorres/ludovico/issues",
+    },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Topic :: Text Processing :: Markup :: LaTeX",
     ],
+    python_requires=">=3.7",
 )
